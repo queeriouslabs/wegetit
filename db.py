c209d0e9f76b8203989a551ad3906f25a17a6913
Claire <geistling@protonmail.com>
2020-05-18 23:23:27 -0700

    Added flask init and tinydb schema

---
+++ db.py
@@ -0,0 +1,52 @@
+from tinydb import TinyDB, Query
+import re
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+def get_db():
+    if 'db' not in g:
+        g.db = TinyDB('db.json')
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+    db_tables = ['threads', 'perspectives', 'paraphrases']
+    for db_table in db_tables:
+        if db_table in db.tables():
+            db.drop_table(db_table)
+
+# threads: {"id": UID, "title" : String, "description" : String},
+    table = db.table('threads')
+    table.insert({'id': True, 'title': True, 'description': True})
+    for row in table:
+        print(row)
+
+# perspectives: {"thread_id": FK_threads-id, "term" : String, "initial_interpretation" : String},
+    table = db.table('perspectives')
+    table.insert({'thread_id': True, 'term': True, 'initial_interpretation': True})
+    for row in table:
+        print(row)
+
+# paraphrases: {"thread_id": FK_threads-id, "paraphrase" : String}
+    table = db.table('paraphrases')
+    table.insert({'thread_id': True, 'paraphrase': True})
+    for row in table:
+        print(row)
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
